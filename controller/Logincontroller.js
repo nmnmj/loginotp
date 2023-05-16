c111fc18efb5d7fc2f6c68b7d8deac06eb870e8e
nmnmj <nmnjay@gmail.com>
2023-05-16 21:17:29 +0530

    corrected

--- controller/Logincontroller.js
+++ controller/Logincontroller.js
@@ -1,61 +1,56 @@
 import otpModel from "../models/otpModel.js";
 import userModel from "../models/userModel.js";
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
-class Logincontroller {
-    static login = async(req, res)=>{
-        const {email, otp} = req.body
-        const userRec = await userModel.findOne({ email });
-        const currentTime = Date.now();
-        try {          
+class LoginController {
+  static login = async (req, res) => {
+    const { email, otp } = req.body;
+    const userRec = await userModel.findOne({ email });
+    const currentTime = Date.now();
 
+    try {
+      if (userRec) {
+        if (userRec.attempt === 5) {
+          const userTime = userRec.time.getTime();
+          const timeDifference = currentTime - userTime;
 
-            if(userRec){
-                if(userRec.attempt == 5){
-                    // console.log("in")
-                    const userTime = userRec.time.getTime();
-                    const timeDifference = currentTime - userTime;
-                    if (timeDifference < 3600000) {   //3600000     60000
-                        const remainingTime = Math.ceil((3600000 - timeDifference) / 1000);
-                        res.status(429).send({ "status": "error", "msg": `Please wait ${remainingTime} seconds to Login again. ${userRec.attempt} attempts already done.` });
-                    }else{
-                        const chk = await otpModel.findOne({email, otp})
-                        if(chk){
-                            const d = await otpModel.deleteMany({email})
-                            await userModel.findOneAndUpdate({ email }, { attempt:0, time: currentTime });
-                            // console.log(userRec._id)
-                            let token = jwt.sign({userID:userRec._id}, process.env.JWT_SECRET_KEY, {expiresIn:"1d"})
-
-                            res.send({"status":"success", token})
-                        }else{
-                            const up = await userModel.findOneAndUpdate({ email }, { attempt:0, time: currentTime });
-                            res.status(401).send({ "status": "failed", "msg": "try getting new otp","attempt":up.attempt });
-                        }
-                    }
-                }else if(userRec.attempt < 5){
-                    const chk = await otpModel.findOne({email, otp})
-                    if(chk){
-                        const d = await otpModel.deleteMany({email})
-                        await userModel.findOneAndUpdate({ email }, { attempt:0, time: currentTime });
-                        let token = jwt.sign({userID:userRec._id}, process.env.JWT_SECRET_KEY, {expiresIn:"1d"})
-
-                        res.send({"status":"success", token})
-
-                    }else{
-                        const up = await userModel.findOneAndUpdate({ email }, { $inc: { attempt: 1 }, time: currentTime });
-                        res.status(401).send({ "status": "failed", "msg": "try getting new otp","attempt":up.attempt });
-                    }
-                }else{
-                    res.status(401).send({ "status": "failed", "msg": "try getting new otp ex" });
-                }
-            }else{
-                res.status(400).send({"status":"failed", "msg":"no user found"})
+          if (timeDifference < 3600000) { // 3600000 milliseconds = 1 hour
+            const remainingTime = Math.ceil((3600000 - timeDifference) / 1000);
+            return res.status(429).send({ "status": "error", "msg": `Please wait ${remainingTime} seconds to Login again. ${userRec.attempt} attempts already done.` });
+          } else {
+            const chk = await otpModel.findOne({ email, otp });
+            if (chk) {
+              await otpModel.deleteMany({ email });
+              await userModel.findOneAndUpdate({ email }, { attempt: 0, time: currentTime });
+              let token = jwt.sign({ userID: userRec._id }, process.env.JWT_SECRET_KEY, { expiresIn: "1d" });
+              return res.send({ "status": "success", token });
+            } else {
+              await userModel.findOneAndUpdate({ email }, { attempt: 0, time: currentTime });
+              return res.status(401).send({ "status": "failed", "msg": "Invalid OTP. Please try again." });
             }
-                
-        } catch (error) {
-            res.status(401).send({ "status": "failed", "msg": "something went wrong" });
+          }
+        } else if (userRec.attempt < 5) {
+          const chk = await otpModel.findOne({ email, otp });
+          if (chk) {
+            await otpModel.deleteMany({ email });
+            await userModel.findOneAndUpdate({ email }, { attempt: 0, time: currentTime });
+            let token = jwt.sign({ userID: userRec._id }, process.env.JWT_SECRET_KEY, { expiresIn: "1d" });
+            return res.send({ "status": "success", token });
+          } else {
+            const up = await userModel.findOneAndUpdate({ email }, { $inc: { attempt: 1 }, time: currentTime });
+            const remainingAttempts = 5 - up.attempt;
+            return res.status(401).send({ "status": "failed", "msg": `Invalid OTP. You have ${remainingAttempts} attempts left.`, "attempt": up.attempt });
+          }
+        } else {
+          return res.status(401).send({ "status": "failed", "msg": "Your account is temporarily blocked. Please try again after 1 hour." });
         }
+      } else {
+        return res.status(400).send({ "status": "failed", "msg": "No user found with the provided email." });
+      }
+    } catch (error) {
+      return res.status(500).send({ "status": "error", "msg": "Something went wrong." });
     }
+  }
 }
 
-export default Logincontroller
+export default LoginController;
